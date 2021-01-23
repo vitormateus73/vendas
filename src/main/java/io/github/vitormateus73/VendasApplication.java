41937ea21031719bd016aa90365ba80249d8e443
Vitor mateus <vitormateus73@gmail.com>
2021-01-23 18:39:35 -0300

    Primeiro Commit
    Primeiro commit

---
+++ src/main/java/io/github/vitormateus73/VendasApplication.java
@@ -0,0 +1,22 @@
+package io.github.vitormateus73;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @GetMapping("/")
+    public String helloworld() {
+        return "hello world";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
+
